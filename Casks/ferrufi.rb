11c7799b23e24a81edca801db58e4a70ececef11
Mustafif <mustafif0929@gmail.com>
2026-01-11 14:47:34 -0500

    update experimental version

--- Casks/ferrufi.rb
+++ Casks/ferrufi.rb
@@ -1,6 +1,7 @@
 cask "ferrufi" do
-  version "0.0.0-exp.2.58824bb"
-  sha256 "64f9d9949db73769d7e601ac80dfa3dce2e91a1a3a604e89755fb7a5f3b33515"
+  version "0.0.0-exp.3.b3a798e"
+
+  sha256 "c6d119671629915a4d618036562cec8952434199b124a69e410fc702f0db491d"
 
   url "https://github.com/Mufi-Lang/Ferrufi/releases/download/experimental/Ferrufi-#{version}-macos.zip"
   name "Ferrufi"
